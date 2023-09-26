c39d69cde80096ab2c08dfa20ef90372a8d11374
Gurnoorsb68 <gurnoorsb68@gmail.com>
2023-09-26 10:00:28 -0400

    cicd pipe

--- app1.py
+++ app1.py
@@ -73,7 +73,7 @@ def get_store_info(name):
             return  x
         return  {"message" : "missing store"} , 404
 
-@app1.get("/store/<string:name>/itemdetails")   #APIs to get Items of particulaar stores
+@app1.get("/store/<string:name>/itemdetails")   #API to get Items of particulaar stores
 def get_item(name):
     for x in stores:
         if x["name"] == name:
